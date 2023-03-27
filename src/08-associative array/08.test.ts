2a5aef22dc6a10fa298067fb58769c6957d9b037
NatKhomich <yalapulya94@mail.ru>
2023-03-27 18:33:30 +0600

    associative array

---
+++ src/08-associative array/08.test.ts
@@ -0,0 +1,31 @@
+
+export type UsersType = {
+    [key: string] : {id: number, name: string}
+}
+
+export let users: UsersType = {
+    '1': {id: 1, name: 'Alex'},
+    '10111': {id: 10111, name: 'Natalia'},
+    '101222': {id: 101222, name: 'Katya'}
+}
+
+beforeEach( () => {
+    users = {
+        '1': {id: 1, name: 'Alex'},
+        '10111': {id: 10111, name: 'Natalia'},
+        '101222': {id: 101222, name: 'Katya'}
+    }
+})
+
+test('should update corresponding user', ()=> {
+    users['1'].name = 'Alexander'
+
+    expect(users['1'].name).toBe('Alexander')
+
+})
+
+test('should delete corresponding user', ()=> {
+    delete users['1']
+
+    expect(users['1']).toBeUndefined()
+})
---
+++ src/08-associative array/08.ts
@@ -0,0 +1,24 @@
+import exp from 'constants';
+
+
+export const usersObj = {
+    '0': 'Alex',
+    '1': 'Natalia',
+    '2': 'Katya'
+}
+
+//работа с ассоциативным массивом
+/*let user = {id: 100500, name: 'Igor'}//доб нового user
+users[user.id] = user
+delete users[user.id]//удаление
+users[user.id].name = 'Victor'*/ //перезаписывание имени
+
+
+export const usersArray = [
+    {id: 1, name: 'Alex'},
+    {id: 10111, name: 'Natalia'},
+    {id: 101222, name: 'Katya'}
+]
+
+//usersArray.find( u => u.id === 1)
+//let usersCopy = [...usersArray, user]
