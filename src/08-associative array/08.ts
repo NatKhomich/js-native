dcf0cc551c1b1a1a6ae01adbd3fec9d32bb07a07
NatKhomich <yalapulya94@mail.ru>
2023-03-27 18:38:56 +0600

    associative array

--- src/08-associative array/08.ts
+++ src/08-associative array/08.ts
@@ -9,7 +9,7 @@ export const usersObj = {
 
 //работа с ассоциативным массивом
 /*let user = {id: 100500, name: 'Igor'}//доб нового user
-users[user.id] = user
+users[user.id] = user// поиск user
 delete users[user.id]//удаление
 users[user.id].name = 'Victor'*/ //перезаписывание имени
 
