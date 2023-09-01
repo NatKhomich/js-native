7586aa6ae9f7c8f8017d241f60bb40fdbad3c8a7
NatKhomich <yalapulya94@mail.ru>
2023-09-01 17:02:23 +0600

    promise

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -62,13 +62,22 @@ console.log(promise1)
 //}
 //run()
 
-
-
-
 //-----------------------------------------------------------------------------------------------------
 
-
-
-
-
-
+// function wait(ms) {
+//     return new Promise( (res) => {
+//         setTimeout(() => { res() }, ms)
+//     })
+// }
+//
+// async function run() {
+//     await wait(1000)
+//     console.log(1)
+//
+//     await wait(1000)
+//     console.log(2)
+//
+//     await wait(1000)
+//     console.log(3)
+// }
+// run()
