b9c894017b830b52c782d57cc6cf63fa23eea914
NatKhomich <yalapulya94@mail.ru>
2023-09-15 16:25:29 +0600

    class

--- src/class oop/classOOP.js
+++ src/class oop/classOOP.js
@@ -29,16 +29,46 @@ function commonHello() {
 }
 
 const userFabric = (name) => {
-    const user = {
+    return {
         name: name,
         site: 'nat-khomich.com',
         dateOfBirth: new Date(1994, 11, 6),
         hello: commonHello
     }
-    return user
 }
 
 const u1 = userFabric('Natalia')
 const u2 = userFabric('Alex')
 u1.hello()
 u2.hello()
+
+
+class UserClass {
+    constructor(name, site, dob) {
+        this.name = name
+        this.site = site
+        this.dob = dob
+    }
+    hello() {
+        console.log(`I am ${this.name} from ${this.site}`)
+    }
+}
+const user1 = new UserClass('nata', '@nat-khomich', new Date(1994, 11, 6))
+const user2 = new UserClass('alex', '@alex-khomich', new Date(1990, 2, 17))
+console.log(user1)
+console.log(user2)
+user1.hello()
+user2.hello()
+
+class Coder extends UserClass { //наследование
+    constructor(name, site, dob, tech) {
+        super(name, site, dob) //конструктор класса UserClass
+        this.tech = tech
+    }
+    coder() {
+        console.log(`I am ${this.name} here is my ${this.tech} code: const sum = (a,b) => a + b`)
+    }
+}
+
+const coder1 = new Coder('katya', '@katya.com', new Date(1989, 12, 8), 'javaScript')
+coder1.coder()
